0643971a58a3228a9f04daf8c49e4fcc7c23c20c
gry <gry@gmail.com>
2021-01-30 15:10:40 +0800

    1

--- AFWKWebView/AppDelegate.m
+++ AFWKWebView/AppDelegate.m
@@ -120,4 +120,22 @@
 }
 
 
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+     if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
+         NSURL *webpageURL = userActivity.webpageURL;
+         NSString *host = webpageURL.host;
+        // 在这里写需要的逻辑，比如跳转到某个详情页
+        if ([host isEqualToString:@"www.aa.tk"]) {
+
+        } else{
+            [[UIApplication sharedApplication] openURL:webpageURL options:@{} completionHandler:^(BOOL success) {
+
+             }];
+        }
+     }
+     return YES;
+ }
+
+
 @end
